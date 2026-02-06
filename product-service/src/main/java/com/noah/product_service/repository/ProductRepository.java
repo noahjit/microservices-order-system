710731f0b83508329d7d1fc32d1175924f57a42e
DESKTOP-BPPB1FJ\noah arrowsmith <noaharrowsmith22@gmail.com>
2026-02-06 17:22:41 +1100

    Fix product-service submodule issue

---
+++ product-service/src/main/java/com/noah/product_service/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.noah.product_service.repository;
+
+import com.noah.product_service.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {}
